d2510241d52b2d5a02601b3e90f3adb919580571
Andrew P. (kensoidev) <65781959+kensoi@users.noreply.github.com>
2022-06-17 02:25:33 +0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,44 @@
+"""
+Корень приложения VKBotKit для работы через сообщество
+"""
+import asyncio
+from os import (
+    environ,
+    getenv
+    )
+from sys import argv
+from dotenv import load_dotenv
+from vkbotkit import Librabot
+from vkbotkit.objects import (
+    #decorators,
+    #filters,
+    enums,
+    #library_module
+)
+
+
+async def main():
+    """
+    главная функция приложения
+    """
+    if "-d" in argv:
+        token = environ['DEBUG_TOKEN']
+        log_level = enums.LogLevel.DEBUG
+
+    else:
+        token = environ['PUBLIC_TOKEN']
+        log_level = enums.LogLevel.INFO
+
+    config_log = getenv("CONFIG_LOG", default = "")
+
+    bot = Librabot(token)
+    bot.toolkit.configure_logger(log_level, "f" in config_log, "c" in config_log)
+    bot.library.import_library()
+
+    # START POLLING
+    await bot.start_polling()
+
+if __name__ == "__main__":
+    load_dotenv()
+    loop = asyncio.new_event_loop()
+    loop.run_until_complete(main())
---
+++ library/get_started_lib.py
@@ -0,0 +1,30 @@
+"""
+Пример плагина для vkbotkit
+"""
+
+from vkbotkit.objects import (
+    decorators,
+    filters,
+    #enums,
+    LibraryModule)
+
+
+HELLO_ME = """
+Hello, world
+Programmed to work and not to feel
+Not even sure that this is real
+"""
+
+
+class Main(LibraryModule):
+    """
+    docstring fix
+    """
+
+    @decorators.callback(filters.IsCommand({"start",}))
+    async def send_hello(self, package):
+        """
+        при получении команды '@your_bot_id start' => отправлять текст HELLO_ME
+        """
+        await package.toolkit.send_reply(package, HELLO_ME)
+        
